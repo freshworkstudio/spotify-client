57bcb9966f3a3828d50b08ae4624904fccf71a42
Gonzalo De Spirito <gonzunigad@gmail.com>
2018-04-15 21:09:14 -0300

    [Search] Log queries

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Contracts\MusicSearchInterface;
+use App\SearchLog;
 use App\Serivices\SpotifyMusicSearch;
 use Illuminate\Http\Request;
 use SpotifyWebAPI\SpotifyWebAPI;
@@ -24,14 +25,8 @@ class SearchController extends Controller
         $this->validate($request, [
             'q' => 'required|min:3'
         ]);
-
-        $tracks = null;
-        if ($request->get('q') === 'most_popular') {
-            $tracks = $musicSearch->charts('CL');
-        } else {
-            $tracks = $musicSearch->search($request->get('q'));
-        }
-
+        $this->logQueryOnDatabase($request);
+        $tracks = $this->getTracksBasedOnQuery($request, $musicSearch);
 
         return $tracks;
     }
@@ -39,5 +34,35 @@ class SearchController extends Controller
     public function getTrack($id, MusicSearchInterface $musicSearch)
     {
         return $musicSearch->getTrack($id);
+    }
+    /**
+     * @param Request $request
+     */
+    public function logQueryOnDatabase(Request $request): void
+    {
+        try {
+            $log = new SearchLog();
+            $log->query = $request->get('q');
+            $log->ip = $request->ip();
+            $log->save();
+        } catch (\Exception $exception) {
+            logger()->error($exception);
+        }
+    }
+    /**
+     * @param Request $request
+     * @param MusicSearchInterface $musicSearch
+     * @return null
+     */
+    public function getTracksBasedOnQuery(Request $request, MusicSearchInterface $musicSearch)
+    {
+        $tracks = null;
+        if ($request->get('q') === 'most_popular') {
+            $tracks = $musicSearch->charts('CL');
+        } else {
+            $tracks = $musicSearch->search($request->get('q'));
+        }
+
+        return $tracks;
     }
 }
